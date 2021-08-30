ee0219f4565bb66b92f581bf96af63d643da1ccd
itzkbp <itzkbp@gmail.com>
2021-08-30 20:55:43 +0545

    Frontend Assembly

---
+++ src/FASM.c
@@ -0,0 +1,117 @@
+#include "include/FASM.h"
+
+#include "include/Stdio.h"
+#include "include/Stdlib.h"
+#include "include/String.h"
+
+char *fasm_compound(AST *ast)
+{
+    char *value = (char *) calloc(1, sizeof(char));
+
+    for(int i = 0; (int)i < ast->children->size; i++)
+    {
+        AST *child_ast = (AST *) ast->children->items[i];
+        char *next_value = fasm(child_ast);
+        value = realloc(value, (strlen(next_value) + 1) * sizeof(char) );
+        strcat(value, next_value);
+    }
+
+    return value;
+}
+
+char *fasm_assignment(AST *ast)
+{
+    char *s = (char *) calloc(1, sizeof(char));
+
+    if(ast->value->type == AST_FUNCTION)
+    {
+        const char *template = ".globl %s\n"
+                               "%s:\n";
+        s = realloc(s, (strlen(template)+ (strlen(ast->name)*2) +1) * sizeof(char));
+        sprintf(s, template, ast->name, ast->name);
+
+        AST *child_ast = ast->value;
+        
+        char *child_ast_value = fasm(child_ast->value);
+        s = realloc(s, (strlen(s) + strlen(child_ast_value) + 1) * sizeof(char));
+        strcat(s, child_ast_value);
+    }
+
+    return s;
+}
+
+char *fasm_variable(AST *ast)
+{
+    return " variable ";
+}
+
+char *fasm_call(AST *ast)
+{
+    char *s = (char *) calloc(1, sizeof(char));
+
+    if(strcmp(ast->name, "return") == 0)
+    {
+        AST *first_arg = (AST *) ast->value->children->size ? ast->value->children->items[0] : (void *) 0;
+
+        const char *template = "movl $%d, %%eax\n"
+                               "ret\n";
+
+        char *ret_str = calloc(strlen(template) + 1, sizeof(char));
+        sprintf(ret_str, template, (first_arg ? first_arg->intValue : 0) );
+
+        s = realloc(s, (strlen(ret_str)+1) *sizeof(char) );
+        strcat(s, ret_str);
+    }
+
+    return s;
+}
+
+char *fasm_int(AST *ast)
+{
+    return " int ";
+}
+
+char *fasm_root(AST *ast)
+{
+    const char *section_text = ".section .text\n"
+                               ".globl _start\n"
+                               "_start:\n"
+                               "call main\n"
+                               "movl %eax, %ebx\n"
+                               "movl $1, %eax\n"
+                               "int $0x80\n\n";
+
+    char *value = (char *) calloc((strlen(section_text) + 1), sizeof(char));
+    strcpy(value, section_text);
+
+    char *next_value = fasm(ast);
+    value = realloc(value, (strlen(section_text) + strlen(next_value) + 1) *sizeof(char) );
+    strcat(value, next_value);
+
+    return value;
+}
+
+char *fasm(AST *ast)
+{
+    char *value = (char *) calloc(1, sizeof(char));
+    char *next_value = 0;
+
+    switch(ast->type)
+    {
+        case AST_COMPOUND:   next_value = fasm_compound(ast);   break;
+        case AST_ASSIGNMENT: next_value = fasm_assignment(ast); break;
+        case AST_VARIABLE:   next_value = fasm_variable(ast);   break;
+        case AST_CALL:       next_value = fasm_call(ast);       break;
+        case AST_INT:        next_value = fasm_int(ast);        break;
+
+        default: {
+            printf("[FASM] :: Unexpected AST of type '%d'\n",ast->type);
+            exit(1);
+        } break;
+    }
+
+    value = realloc(value, ( strlen(next_value) + 1 ) * sizeof(char));
+    strcat(value, next_value);
+
+    return value;
+}
--- src/include/FASM.h
+++ src/include/FASM.h
@@ -1,3 +1,14 @@
 #ifndef ASC_FASM_H
     #define ASC_FASM_H
+
+    #include "AST.h"
+
+    char *fasm_compound  (AST *ast);
+    char *fasm_assignment(AST *ast);
+    char *fasm_call      (AST *ast);
+    char *fasm_variable  (AST *ast);
+    char *fasm_int       (AST *ast);
+    char *fasm           (AST *ast);
+    char *fasm_root      (AST *ast);
+
 #endif
