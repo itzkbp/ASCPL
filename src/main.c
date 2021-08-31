2d542b6e4aaf0c3e92d25594b9842fea41bb90be
itzkbp <itzkbp@gmail.com>
2021-08-31 09:40:12 +0545

    message edited

--- src/main.c
+++ src/main.c
@@ -1,15 +1,21 @@
 #include "include/ASC.h"
 
 #include "include/Stdio.h"
+#include "include/Stdlib.h"
+#include "include/String.h"
 
 int main(int argc, char *argv[])
 {
     if(argc < 2)
     {
-        printf("No input file");
+        printf("\n\t\t#####################--------------------------------------------\n");
+        printf(  "\t\t##  No input file  ## ---->    use =>      ./asc fileName.asc    |");
+        printf("\n\t\t#####################--------------------------------------------\n\n");
+        printf("\t\tUse the file in examples. e.g       .asc ./examples/ex-1.asc\n\n");
+
         return 1;
     }
-
-    asc_compile_file(argv[1]);
+    system("mkdir bin");
+    asc_compile_file(argv[1], argc > 2 && (strcmp(argv[2], "--assembly") == 0) ? 1 : 0);
     return 0;
 }
