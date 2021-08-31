97055382d0bd470c3fd2c1ed76fd1d4fc10ee09a
itzkbp <itzkbp@gmail.com>
2021-08-31 09:39:25 +0545

    ambuigity in list fixed

--- src/include/List.h
+++ src/include/List.h
@@ -11,26 +11,8 @@
         size_t item_size;
     } List;
     
-    List *init_list(size_t item_size)
-    {
-        List *list = (List *) calloc(1, sizeof(List));
-        list->size  = 0;
-        list->item_size = item_size;
-        list->items = 0;
+    List *init_list(size_t item_size);
 
-        return list;
-    }
-
-    void list_push(List *list, void *item)
-    {
-        list->size += 1;
-
-        if(!list->items)
-            list->items = calloc(1, list->item_size);
-        else
-            list->items = realloc(list->items, (list->size * list->item_size));
-    
-        list->items[list->size - 1] = item;
-    }
+    void list_push(List *list, void *item);
     
 #endif
