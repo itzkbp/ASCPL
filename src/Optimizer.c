b0cc7b6dd96c108c477a53aecc9cb404790b58ef
itzkbp <itzkbp@gmail.com>
2021-09-01 12:34:11 +0545

    For OPTIMIZING AST

---
+++ src/Optimizer.c
@@ -0,0 +1,2 @@
+#include "include/Optimizer.h"
+
---
+++ src/include/Optimizer.h
@@ -0,0 +1,9 @@
+#ifndef ASC_OPTIMIZER_H
+    #define ASC_OPTIMIZER_H
+
+    typedef struct st_Optimizer
+    {
+        int a;
+    } Optimizer;
+
+#endif
